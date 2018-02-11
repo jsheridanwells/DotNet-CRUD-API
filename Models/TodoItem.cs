b3c1873917d7ca3c669d1cc4e7e382ff15cf2067
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2018-02-11 16:18:07 -0600

    created model and db schema

---
+++ Models/TodoItem.cs
@@ -0,0 +1,9 @@
+namespace DotNetCRUD_API.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool Done { get; set; }
+    }
+}

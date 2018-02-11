ece8a401caae140d33eab364dad6a45342a4df35
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2018-02-11 16:18:37 -0600

    created model and schema

--- Startup.cs
+++ Startup.cs
@@ -8,21 +8,18 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using DotNetCRUD_API.Models;
 
 namespace DotNetCRUD_API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
             services.AddMvc();
         }
 
